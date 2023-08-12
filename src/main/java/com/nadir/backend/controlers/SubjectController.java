b4035d76e5fdcf2964099f12794d016ae8c76aaf
Anas Bensaid <bensaidanass9@gmail.com>
2023-08-12 15:18:29 +0100

    Adding Get All Subjects

--- src/main/java/com/nadir/backend/controlers/SubjectController.java
+++ src/main/java/com/nadir/backend/controlers/SubjectController.java
@@ -1,15 +1,23 @@
 package com.nadir.backend.controlers;
 
 import com.nadir.backend.models.Subject;
+import com.nadir.backend.models.Teacher;
 import com.nadir.backend.services.SubjectService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/subjects")
 public class SubjectController {
     private final SubjectService subjectService;
+
+    @GetMapping
+    public List<Subject> getAllSubjects() {
+        return subjectService.getAllSubjects();
+    }
 
     @PostMapping
     public Subject create(@RequestBody Subject subject) {
--- src/main/java/com/nadir/backend/services/SubjectService.java
+++ src/main/java/com/nadir/backend/services/SubjectService.java
@@ -2,18 +2,27 @@ package com.nadir.backend.services;
 
 import com.nadir.backend.models.Major;
 import com.nadir.backend.models.Subject;
+import com.nadir.backend.models.Teacher;
 import com.nadir.backend.repositories.MajorRepository;
 import com.nadir.backend.repositories.SubjectRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 public class SubjectService {
     private final SubjectRepository subjectRepository;
+
+    public List<Subject> getAllSubjects() {
+        return subjectRepository.findAll();
+    }
+
     public Subject createSubject(Subject subject) {
         return subjectRepository.save(subject);
     }
+
     public Subject getSubjectById(Long id) {
         return subjectRepository.findById(id).orElse(null);
     }
